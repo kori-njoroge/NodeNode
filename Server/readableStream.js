32ccda5cac8d845ccf8014b7c737b1860fcdc5f1
Kori Njoroge <75626465+kori-njoroge@users.noreply.github.com>
2022-09-04 17:36:08 +0300

    Delete Server directory

--- Server/readableStream.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs')
-
-const myrStream = fs.createReadStream(__dirname + '/elephant.txt', 'utf8')
-
-myrStream.on('data', (chunk) =>{
-    console.log('/************New Chunk****************/');
-    console.log(chunk)
-})
